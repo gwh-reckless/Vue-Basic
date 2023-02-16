a95a676f189ca2cdca069e872833ff139c2c97f2
gwh-reckless <williamguogo@gmail.com>
2023-02-16 08:23:17 +0800

    5. Use methods properties

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const app = Vue.createApp({
       age: '1000',
     };
   },
+  //   methods object specify the methods can use inside div#app
+  methods: {
+    increaseAge() {
+      // must use this keyword
+      this.age++;
+    },
+    decreaseAge() {
+      this.age--;
+    },
+    changeTitle(title) {
+      this.title = title;
+    },
+  },
 });
 
 app.mount('#app');
