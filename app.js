a8be62581c2b2590ac63e19a0d0a58e1b50fdb62
gwh-reckless <williamguogo@gmail.com>
2023-02-21 01:42:31 +0800

    9. Attribute Binding

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
+      url: 'http://guoweihao.cn',
       books: [
-        { title: 'name of the wind', author: 'patric rothfuss' },
-        { title: 'the way of kings', author: 'brandon  sanderson' },
-        { title: 'the final empire', author: 'brandon sanderson' },
+        {
+          img: 'assets/1.png',
+          title: 'name of the wind',
+          author: 'patric rothfuss',
+        },
+        {
+          img: 'assets/2.jpg',
+          title: 'the way of kings',
+          author: 'brandon  sanderson',
+        },
+        {
+          img: 'assets/3.jpg',
+          title: 'the final empire',
+          author: 'brandon sanderson',
+        },
       ],
     };
   },
