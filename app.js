0693f13a3d1ea09b717ebc36cc1f5930bf11413f
gwh-reckless <williamguogo@gmail.com>
2023-02-21 01:51:21 +0800

    Dynamic Classes

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ const app = Vue.createApp({
           img: 'assets/1.png',
           title: 'name of the wind',
           author: 'patric rothfuss',
+          isFav: true,
         },
         {
           img: 'assets/2.jpg',
           title: 'the way of kings',
           author: 'brandon  sanderson',
+          isFav: true,
         },
         {
           img: 'assets/3.jpg',
           title: 'the final empire',
           author: 'brandon sanderson',
+          isFav: false,
         },
       ],
     };
