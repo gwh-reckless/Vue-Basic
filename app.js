6aeb187d3c07966105df72b357c5851a4f4871a7
gwh-reckless <williamguogo@gmail.com>
2023-02-21 01:33:11 +0800

    8. v-for

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
 const app = Vue.createApp({
-  // the template will override everything inside div#app
   data() {
     return {
       showBooks: true,
-      title: 'Qiangjinjiu',
-      author: 'Libai',
-      age: '1000',
-      x: 0,
-      y: 0,
+      books: [
+        { title: 'name of the wind', author: 'patric rothfuss' },
+        { title: 'the way of kings', author: 'brandon  sanderson' },
+        { title: 'the final empire', author: 'brandon sanderson' },
+      ],
     };
-  },
-  //   methods object specify the methods can use inside div#app
-  methods: {
-    increaseAge() {
-      // must use this keyword
-      this.age++;
-    },
-    decreaseAge() {
-      this.age--;
-    },
-    changeTitle(title) {
-      this.title = title;
-    },
-    changeToggle() {
-      this.showBooks = !this.showBooks;
-    },
-    handleEvent(e, data) {
-      console.log(e, e.type);
-      if (data) {
-        console.log(data);
-      }
-    },
-    handleMouseMove(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
-    },
   },
 });
 
